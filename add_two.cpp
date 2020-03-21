0fd032c56869bc2abc2e4c580a8f753ebfbd19d1
Baiqi <zhangshuo023@163.com>
2020-03-21 22:14:17 +0800

    sum of two numbers

---
+++ add_two.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(void){
+	int v1 , v2;
+	std::cout<<"Please enter two numbers"<<std::endl;
+	std::cin>>v1>>v2;
+	std::cout<<"the sum of "<<v1<<" and "<< v2 <<" is " << v1 + v2 << std::endl;
+
+	return 0;
+}
